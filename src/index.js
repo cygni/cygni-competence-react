1331dbba81421695df0b6d0b26662c03b7905eac
Fredrik Edgren <fredrik.edgren@svt.se>
2016-01-17 23:01:35 +0100

    All Containers an lineheight in title

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import HardCoded from './containers/HardCoded';
+//import RandomData from './containers/RandomData';
+//import ApiData from './containers/ApiData';
 //import App from './containers/App';
 
 import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
@@ -10,5 +12,13 @@ render(
 );
 
 //render(
-//  <App/>, document.getElementById('app')
+//  <RandomData/>, document.getElementById('app')
+//);
+
+//render(
+//  <ApiData/>, document.getElementById('app')
+//);
+
+//render(
+// <App/>, document.getElementById('app')
 //);
