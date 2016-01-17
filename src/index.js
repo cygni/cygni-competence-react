6b09a12bc5ca3150b8affba861e360ace2dfb83b
Fredrik Edgren <fredrik.edgren@svt.se>
2016-01-17 22:44:57 +0100

    App Container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 import HardCoded from './containers/HardCoded';
-import App from './containers/App';
+//import App from './containers/App';
 
 import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
 render(
   <HardCoded/>, document.getElementById('app')
 );
+
+//render(
+//  <App/>, document.getElementById('app')
+//);
